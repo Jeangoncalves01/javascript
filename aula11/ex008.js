fd3589dca23fbb6bff87072da96661b6bd6f9d47
Jeangoncalves <jeangoncalves008@gmail.com>
2026-05-19 17:10:39 -0300

    exercício JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+
+var velb = 61
+console.log(`A velocidade do seu carro é ${velb} km/h`)
+if (velb > 60) { // Condição simples
+    console.log(`Você ultrapassou velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre com segurança!`)
